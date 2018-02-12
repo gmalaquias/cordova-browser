ed634d3ec1a96ef95f62c4669971ebcc406c4b1e
Gabriel Malaquias <gabriel.malaquias@linx.com.br>
2018-02-12 08:55:51 -0200

    Ajuste na abertura de janela no IOS

--- www/js/index.js
+++ www/js/index.js
@@ -45,13 +45,13 @@ var app = {
         if(Connection.NONE == networkState){
             var el = document.getElementById('msg'); 
             el.innerHTML = 'Dispositivo sem conexão com a internet.'
-            setInterval(function(){
+            setInterval(function(){ 
                 location.reload();
             },2000);
         }else{
             setTimeout(function(){
-                var _iabRef = window.open("https://prd-apw-postofacilmobile.azurewebsites.net", '_self', 'location=no');
-                    _iabRef.addEventListener('loadstop', function(event) {    
+                var _iabRef = window.open("https://prd-apw-postofacilmobile.azurewebsites.net", '_blank', 'location=no');
+                _iabRef.addEventListener('loadstop', function(event) {    
                 });
             },1500);
         }
